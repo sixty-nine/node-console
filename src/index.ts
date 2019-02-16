0ea706a8a7359eac3f2b7d843e88f9e29f9993e8
Daniele Barsotti <daniele.barsotti@energy.ch>
2019-02-16 15:18:02 +0100

    Command line args parsing

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import figlet from 'figlet/lib/node-figlet';
 import { terminal as term } from 'terminal-kit';
 import node_fetch from 'node-fetch';
 import ora from 'ora';
+import minimist from 'minimist';
 
 const wttrIn = async (city: string, format = 4): Promise<string> => {
   const data = await node_fetch(`https://wttr.in/${city}?silent_lang=fr&format=${format}`)
@@ -34,17 +35,37 @@ void async function () {
     .nextLine(2)
   ;
 
+  const { _: cmd, ...args } = minimist(process.argv.slice(2));
+
+  if (cmd[0] === 'help' || args.h || args.help) {
+    console.log(`
+    splash <options>
+
+    --title ............ Title text
+    --image ............ Image path
+    --cities ........... Coma separated list of cities for weather forecast
+    --version, -v ...... Show version
+    --help, -h ......... Show this help
+    `);
+    process.exit(1);
+  } else if (args.v || args.version) {
+    const { version } = require(__dirname + '/../../package.json');
+    console.log(version);
+    process.exit(0);
+  }
+
   const imageWidth = 20;
   const colOffset = 2;
-  const title = 'Welcome!';
-  const image = '/home/dev/dan-console/_data/avatar.400x400.jpeg';
+  const title = args.title || 'Welcome!';
+  const image = args.image || __dirname + '/../../assets/avatar.400x400.jpeg';
 
   const spinner = ora().start();
   spinner.color = 'blue';
   spinner.text = 'Loading...';
 
   try {
-    const forecast = await weatherIn(['Fribourg', 'Bern', 'Zurich']);
+    const cities = args.cities ? args.cities.split(',') : ['Fribourg', 'Bern', 'Zurich'];
+    const forecast = await weatherIn(cities);
     const options = { weekday: 'long', year: 'numeric', month: 'numeric', day: 'numeric' };
     const info = [`Date: ${new Date().toLocaleDateString('fr-CH', options)}`].concat(forecast);
     const data = figlet.textSync(title);
@@ -55,6 +76,13 @@ void async function () {
 
     spinner.stop();
     term.fullscreen(true);
+    term.nextLine(1);
+
+    info.forEach(f => term
+      .right(imageWidth + colOffset)
+      .bgBlue(center(f.padEnd(25), ' ', rightColWidth))
+      .nextLine(1)
+    );
 
     lines.forEach(line => {
       titleWidth = line.length;
@@ -64,12 +92,6 @@ void async function () {
         .nextLine(1)
       ;
     });
-
-    info.forEach(f => term
-      .right(imageWidth + colOffset)
-      .bgBlue(center(f.padEnd(25), ' ', rightColWidth))
-      .nextLine(1)
-    );
 
     term.styleReset().nextLine(2);
 
